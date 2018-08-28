02a9b0b3467f53598c107b915817c63da62d5895
HARSH <HARSHBHAMA96@GMAIL.COM>
2018-08-28 21:20:47 +0530

    add likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'likes/toggle'
   resources :posts
   devise_for :users
 
@@ -12,12 +13,16 @@ Rails.application.routes.draw do
   end
 end
 
+
   post 'image/create', to: 'images#create', as: :images
   post 'comment/create', to: 'comments#create', as: :comments
 
   delete 'comments/:id', to: 'comments#destroy', as: :comments_delete
 
   delete 'images/:id', to: 'images#destroy', as: :images_delete
+
+    post 'likes/toggle/:post_id', to: 'likes#toggle', as: :toggle_like
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
